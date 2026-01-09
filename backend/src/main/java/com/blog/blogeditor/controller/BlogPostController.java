94ea619e3df435eeeb66372c623b46eee47a3a39
your name <rushi@gmail>
2026-01-09 13:33:17 +0530

    Security: externalize DB password and add global CORS config

--- backend/src/main/java/com/blog/blogeditor/controller/BlogPostController.java
+++ backend/src/main/java/com/blog/blogeditor/controller/BlogPostController.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/posts")
-@CrossOrigin(origins = "http://localhost:4200")
+
 public class BlogPostController {
 
     private final BlogPostRepository blogPostRepository;
